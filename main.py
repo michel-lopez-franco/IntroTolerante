f27e8c38e2f6321b526711e07be957ac700b0fa1
michel-lopez-franco <michel.lopez@academicos.udg.mx>
2026-01-21 12:27:22 -0600

    eliminar función suma y ajustar importación

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import suma
+
 print("hola mundo")
 print("¡Bienvenido a Python!")
 
 
-def suma(a, b):
-    return a + b
-
-
 def resta(a, b):
     return a - b
+
+
+print("La resta de 5 y 3 es:", resta(5, 3))
+print("La suma de 5 y 3 es:", suma.suma(5, 3))
